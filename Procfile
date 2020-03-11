481317a5676affa0bcaf6cb6319231dd1ac6328f
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-03-11 02:53:35 -0400

    procfile updated

--- Procfile
+++ Procfile
@@ -1,2 +1,4 @@
 web: gunicorn app:app
-release: python manage.py db init
+python manage.py db init
+python manage.py db migrate
+python manage.py db upgrade
--- manage.py
+++ manage.py
@@ -1,10 +1,11 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app import app, db
 
+from app import app
+from models import db
 
-manager = Manager(app)
 migrate = Migrate(app, db)
+manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
